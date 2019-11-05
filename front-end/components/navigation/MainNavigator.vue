7af43a9c28d66978c4f8e3b84ba35909c68912bc
Eduardo Lehrbach <eduardo.lehrbach@gmail.com>
2019-11-05 19:19:06 -0300

    Add MainNavigator component

---
+++ front-end/components/navigation/MainNavigator.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-bottom-navigation grow color="teal" class="my-bar">
+    <v-btn class="main-btn">
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+
+    <v-btn class="main-btn mx-10">
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.my-bar {
+  position: fixed;
+  bottom: 0;
+}
+</style>
