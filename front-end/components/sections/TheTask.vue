c6e0956f995756425bfc4cd1cc55291ec84a6c78
Eduardo Lehrbach <eduardo.lehrbach@gmail.com>
2019-11-05 19:18:28 -0300

    Add TheTask component

---
+++ front-end/components/sections/TheTask.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="col-1">
+        <v-btn text icon color="black">
+          <v-icon>mdi-record-circle-outline</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col class="d-flex align-center">
+        <span class="caption">{{ tasks[0].title }}</span>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TheTask',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 0,
+          title: 'Terminar componente TheTask'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped></style>
