256c876db98fe3f72742b8e30e18f275cbd7d324
Eduardo Lehrbach <eduardo.lehrbach@gmail.com>
2019-11-05 19:16:48 -0300

    Adiciona componente inicial TaskList

---
+++ front-end/components/sections/TaskList.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <h1 class="headline">My Tasks</h1>
+        <the-task />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import TheTask from '@/components/sections/TheTask'
+export default {
+  components: { TheTask }
+}
+</script>
+
+<style></style>
