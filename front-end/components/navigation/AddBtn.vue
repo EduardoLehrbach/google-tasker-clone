a6334dc66e273ffe245dc85da499943f5c7218a4
Eduardo Lehrbach <eduardo.lehrbach@gmail.com>
2019-11-05 19:18:02 -0300

    Adds AddBtn component

---
+++ front-end/components/navigation/AddBtn.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="d-flex">
+    <v-btn flat icon elevation-2>
+      <v-icon x-large>mdi-plus-circle </v-icon>
+    </v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'AddBtn'
+}
+</script>
+
+<style scoped></style>
